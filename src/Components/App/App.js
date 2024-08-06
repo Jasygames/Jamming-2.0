a1f3137e1d23914340d1f9fef075fa030e794ee1
Jasy Games <169844196+Jasygames@users.noreply.github.com>
2024-08-06 08:12:25 +0100

    Frist Commit fixed

---
+++ src/Components/App/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+    <h1>
+      Jammming
+    </h1>
+    <div className="App">
+      {/* <!-- Add a SearchBar component --> */}
+      
+      <div className="App-playlist">
+        {/* <!-- Add a SearchResults component --> */}
+        {/* <!-- Add a Playlist component --> */}
+      </div>
+    </div>
+  </div>
+  );
+}
+
+export default App;
